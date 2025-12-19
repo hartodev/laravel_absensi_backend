3b23fc60b6b2929405c952e64072b89583892c12
hartodev <rudidevlopment@gmail.com>
2025-12-19 14:33:17 +0700

    perbaikan role

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -9,11 +9,48 @@ class RoleMiddleware
 {
  public function handle($request, Closure $next, $role)
     {
+        // if (!Auth::check()) {
+        //     return redirect('/'); // belum login
+        // }
+
+        // if (Auth::user()->role !== $role) {
+        //     abort(403, 'Akses ditolak.');
+        // }
+
+        // return $next($request);
+
+
+        // versi web dan api
+
+          // =========================
+        // 1. BELUM LOGIN
+        // =========================
         if (!Auth::check()) {
-            return redirect('/'); // belum login
+
+            // API request
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Unauthenticated'
+                ], 401);
+            }
+
+            // WEB request
+            return redirect()->route('login.form');
         }
 
+        // =========================
+        // 2. ROLE TIDAK SESUAI
+        // =========================
         if (Auth::user()->role !== $role) {
+
+            // API request
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Forbidden. Role tidak diizinkan.'
+                ], 403);
+            }
+
+            // WEB request
             abort(403, 'Akses ditolak.');
         }
 
